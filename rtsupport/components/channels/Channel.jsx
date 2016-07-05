2b49226a3a8b1395414220e4c2ff437420d3d385
Fareez Ahmed <fareez.ahmed@ge.com>
2016-07-05 07:49:56 +0600

    formatting

--- rtsupport/components/channels/Channel.jsx
+++ rtsupport/components/channels/Channel.jsx
@@ -1,27 +1,27 @@
 import React, { Component } from 'react';
 class Channel extends Component {
-	onClick(e) {
-		e.preventDefault();
-		const { setChannel, channel } = this.props;
-		setChannel(channel);
-	}
-	render() {
-		const { channel } = this.props;
-		return (
-			<li>
+  onClick(e) {
+    e.preventDefault();
+    const { setChannel, channel } = this.props;
+    setChannel(channel);
+  }
+  render() {
+    const { channel } = this.props;
+    return (
+      <li>
 				<a onClick={this.onClick.bind(this)}>
 					{channel.name}
 				</a>
 			</li>
-		)
-	}
+    )
+  }
 }
 
 //lets you know which props this component accepts and whether required or not.
 
 Channel.propTypes = {
-	channel: React.PropTypes.object.isRequired,
-	setChannel: React.PropTypes.func.isRequired
+  channel: React.PropTypes.object.isRequired,
+  setChannel: React.PropTypes.func.isRequired
 }
 
 export default Channel;

ec5c2e5290e4dcbff488ffdcb97238cfc4f94cb5
Fareez Ahmed <fareez.ahmed@ge.com>
2016-07-06 09:04:09 +0600

    formatting and misc

--- rtsupport/components/users/UserForm.jsx
+++ rtsupport/components/users/UserForm.jsx
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 
 class UserForm extends Component {
-	onSubmit (e) {
-		e.preventDefault();
-		const node = this.refs.userName;
-		const userName = node.value;
-		this.props.setUserName(userName)
-		node.value = "";
-	}
-	render () {
-		return (
-			<form onSubmit={this.onSubmit.bind(this)}>
+  onSubmit(e) {
+    e.preventDefault();
+    const node = this.refs.userName;
+    const userName = node.value;
+    this.props.setUserName(userName)
+    node.value = "";
+  }
+  render() {
+    return (
+      <form onSubmit={this.onSubmit.bind(this)}>
 				<div className="form-group">
 					<input
 						className="form-control"
@@ -20,12 +20,12 @@ class UserForm extends Component {
 					/>
 				</div>
 			</form>
-		)
-	}
+    )
+  }
 }
 
 UserForm.propTypes = {
- setUserName: React.PropTypes.func.isRequired
+  setUserName: React.PropTypes.func.isRequired
 }
 
 export default UserForm

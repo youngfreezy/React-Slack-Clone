42e850c8408977e9114cfda9850805fee5c41527
Fareez Ahmed <fareez.ahmed@ge.com>
2016-07-06 09:15:36 +0600

    formatting

--- rtsupport/socket.js
+++ rtsupport/socket.js
@@ -1,43 +1,37 @@
-import { EventEmitter } from 'events';
+import {EventEmitter} from 'events';
 
 class Socket {
-  constructor(ws = new WebSocket(), ee = new EventEmitter()) {
+  constructor(ws = new WebSocket(), ee = new EventEmitter()){
     this.ws = ws;
     this.ee = ee;
     ws.onmessage = this.message.bind(this);
     ws.onopen = this.open.bind(this);
     ws.onclose = this.close.bind(this);
-
-    on(name, fn) {
-    	this.ee.on(name, fn);
-    } 
-
-    off(name, fn) {
-    	this.ee.removeListener(name, fn);
-    } 
-
-    emit(name, data){
-    	const message = JSON.stringify({name, data});
-    	this.ws.send(message);
+  }
+  on(name, fn){
+    this.ee.on(name, fn);
+  }
+  off(name, fn){
+    this.ee.removeListener(name, fn);
+  }
+  emit(name, data){
+    const message = JSON.stringify({name, data});
+    this.ws.send(message);
+  }
+  message(e){
+    try{
+      const message = JSON.parse(e.data);
+      this.ee.emit(message.name, message.data);
     }
-    message(e) {
-     try {
-     	const message = JSON.parse(e.data);
-     	this.ee.emit(message.name, message.data);
-     }
-
-     catch (err) {
-     		this.ee.emit('error', err);
-     }
+    catch(err){
+      this.ee.emit('error', err);
     }
-
-    open() {
-      this.ee.emit('connect');
-    }
-
-    close() {
-      this.ee.emit('disconnect');
-    }
+  }
+  open(){
+    this.ee.emit('connect');
+  }
+  close(){
+    this.ee.emit('disconnect');
   }
 }
 
